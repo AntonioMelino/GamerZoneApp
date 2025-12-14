0fee7c783549f0f7af61dba70e7276c6eddc8bec
AntonioMelino <antoniomelino1997@gmail.com>
2025-12-14 11:23:54 -0300

    style(cart-widget): agregar estilos y actualizar JSX del componente
    -Se añadió el archivo CSS del componente CartWidget y se realizaron ajustes en su JSX.

--- src/components/common/cartWidget/CartWidget.jsx
+++ src/components/common/cartWidget/CartWidget.jsx
@@ -1,21 +1,34 @@
-import CartIcon from '@mui/icons-material/ShoppingCart';
-import { Badge } from '@mui/material';
-import { useContext } from 'react';
-import { CartContext } from '../../../context/CartContext';
+"use client";
+
+import CartIcon from "@mui/icons-material/ShoppingCart";
+import { Badge } from "@mui/material";
+import { useContext } from "react";
+import { CartContext } from "../../../context/CartContext";
+import "./CartWidget.css";
 
 const CartWidget = () => {
-  const {getTotalQuantity} = useContext(CartContext);
-  const total = getTotalQuantity()
+  const { getTotalQuantity } = useContext(CartContext);
+  const total = getTotalQuantity();
 
   return (
-    <div>
-
-        <Badge badgeContent={total} color="secondary" showZero={true}>
-          <CartIcon/>
-        </Badge>
-
+    <div className="cart-widget-container">
+      <Badge
+        badgeContent={total}
+        color="secondary"
+        showZero={true}
+        className="cart-widget-badge"
+        sx={{
+          "& .MuiBadge-badge": {
+            backgroundColor: "var(--secondary-color)",
+            color: "white",
+            fontWeight: 700,
+          },
+        }}
+      >
+        <CartIcon className="cart-widget-icon" />
+      </Badge>
     </div>
-  )
-}
+  );
+};
 
-export default CartWidget
+export default CartWidget;
