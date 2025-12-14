3981cfe392640ccafacbb20b83111c12326af5a4
AntonioMelino <antoniomelino1997@gmail.com>
2025-12-14 11:30:45 -0300

    style(footer): actualizar estilos del footer
    -Se realizaron ajustes visuales en el CSS del footer.

--- src/components/layouts/footer/Footer.jsx
+++ src/components/layouts/footer/Footer.jsx
@@ -1,4 +1,4 @@
-import "./Footer.css"; // Archivo de estilos para el footer
+import "./Footer.css";
 import FacebookIcon from "@mui/icons-material/Facebook";
 import TwitterIcon from "@mui/icons-material/Twitter";
 import InstagramIcon from "@mui/icons-material/Instagram";
