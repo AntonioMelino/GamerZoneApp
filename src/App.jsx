f08e0d5f30e59b05027073fbac5f9700b74ccd3c
AntonioMelino <antoniomelino1997@gmail.com>
2025-12-14 12:58:10 -0300

    feat(app): actualizar configuración y agregar rutas y contextos globales
    -Se actualizaron los archivos package.json y firebaseConfig.
    -Se añadieron las rutas de Profile y Login.
    -Se incorporaron los contextos globales de autenticación y tema.

--- src/App.jsx
+++ src/App.jsx
@@ -3,37 +3,50 @@ import Cart from "./components/pages/cart/Cart";
 import ItemDetail from "./components/pages/itemDetail/ItemDetail";
 import ItemListContainer from "./components/pages/itemListContainer/ItemListContainer";
 import Chekout from "./components/pages/checkout/Chekout";
+import Login from "./components/pages/login/Login";
+import Profile from "./components/pages/profile/Profile";
 
 import { BrowserRouter, Routes, Route } from "react-router";
 import PageNotFound from "./components/pages/pageNotFound/PageNotFound";
 import CartContextProvider from "./context/CartContext";
 import Footer from "./components/layouts/footer/Footer";
+import { ThemeProvider } from "./context/ThemeContext";
+import { AuthProvider } from "./context/AuthContext";
 
 function App() {
   return (
     <BrowserRouter>
-      <CartContextProvider>
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            minHeight: "100vh",
-          }}
-        >
-          <NavBar />
-          <main style={{ flex: 1 }}>
-            <Routes>
-              <Route path="/" element={<ItemListContainer />} />
-              <Route path="/category/:name" element={<ItemListContainer />} />
-              <Route path="/cart" element={<Cart />} />
-              <Route path="/itemDetail/:id" element={<ItemDetail />} />
-              <Route path="/checkout" element={<Chekout />} />
-              <Route path="*" element={<PageNotFound />} />
-            </Routes>
-          </main>
-          <Footer />
-        </div>
-      </CartContextProvider>
+      <ThemeProvider>
+        <AuthProvider>
+          <CartContextProvider>
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                minHeight: "100vh",
+              }}
+            >
+              <NavBar />
+              <main style={{ flex: 1 }}>
+                <Routes>
+                  <Route path="/" element={<ItemListContainer />} />
+                  <Route
+                    path="/category/:name"
+                    element={<ItemListContainer />}
+                  />
+                  <Route path="/cart" element={<Cart />} />
+                  <Route path="/itemDetail/:id" element={<ItemDetail />} />
+                  <Route path="/chekout" element={<Chekout />} />
+                  <Route path="/login" element={<Login />} />
+                  <Route path="/profile" element={<Profile />} />
+                  <Route path="*" element={<PageNotFound />} />
+                </Routes>
+              </main>
+              <Footer />
+            </div>
+          </CartContextProvider>
+        </AuthProvider>
+      </ThemeProvider>
     </BrowserRouter>
   );
 }
--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { createContext, useContext, useState, useEffect } from "react";
-import { auth } from "../firebase/config";
+import { auth } from "../firebaseConfig";
 import {
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
